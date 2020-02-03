9336e3e4ae5f27dc7e91a4493d9a6b963b632a34
taroj khan <tarujkhan@yahoo.com>
2020-02-03 23:53:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog 
+  @@name = Fido
+  def name=(name)
+    @@name
+  end
+
+ 
+  def name
+    return @name
+  end
+
+
+def bark 
+  bark = "woof!"
+end 
+bark
+end
